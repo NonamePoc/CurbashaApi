75583e1c9b9fe53a142e4cd3e138f174093d73f2
KostyaHeshur <74602218+KostyaHeshur@users.noreply.github.com>
2022-11-02 12:07:11 +0200

    auth-update/backet-frontend-add/img-bug-fix

---
+++ CurbashaApi/Controllers/BasketProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CurbashaApi.Controllers
+{
+    public class BasketProductController : Controller
+    {
+        public IActionResult Basket()
+        {
+            return View();
+        }
+    }
+}
