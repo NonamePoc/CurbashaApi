5359b34deabe6f66465155aa22f7e8feed3b6041
bittersweet-01 <syrotynska.viktoriya@student.uzhnu.edu.ua>
2022-11-02 10:57:33 +0200

    product&shop pages

--- CurbashaApi/Controllers/ShopController.cs
+++ CurbashaApi/Controllers/ShopController.cs
@@ -1,12 +1,62 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CurbashaApi.Areas.Identity.Entity;
+using CurbashaApi.Data;
+using CurbashaApi.Models;
+using CurbashaApi.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 namespace CurbashaApi.Controllers
 {
     public class ShopController : Controller
     {
-        public IActionResult Shop()
+        private readonly CurbashaApiContext _context;
+
+        public ShopController(CurbashaApiContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        public async Task<IActionResult> Shop()
+        {
+            var allProducts = _context.AspProducts.Select(p => p);
+            var allSelections = _context.AspSelections.Select(s => s);
+
+            var shopVM = new ShopViewModel
+            {
+                Products = await allProducts.ToListAsync(),
+                Selections = await allSelections.ToListAsync()
+            };
+            return View(shopVM);
+        }
+
+        public IActionResult Product(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Home", "Home");
+            }
+
+            var selectedProduct = _context.AspProducts.FirstOrDefault(p => p.Id == id);
+
+            if (selectedProduct == null)
+            {
+                return RedirectToAction("Home", "Home");
+            }
+
+            var selectedSection = _context.AspSelections.FirstOrDefault(s => s.Id == selectedProduct.SelectionId);
+
+            
+            string[] imagePathes = new string[2];
+            imagePathes[0] = $"~/images/products/{selectedSection.SelectionName.ToLower()}-{id}.1.jpg";
+            imagePathes[1] = $"~/images/products/{selectedSection.SelectionName.ToLower()}-{id}.2.jpg";
+            ProductViewModel product = new ProductViewModel()
+            {
+                Product = selectedProduct,
+                SectionName = selectedSection.SelectionName,
+                ImagePathes = imagePathes
+            };
+            return View(product);
         }
     }
 }
---
+++ CurbashaApi/ViewModel/ProductViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using CurbashaApi.Areas.Identity.Entity;
+
+namespace CurbashaApi.ViewModel
+{
+    public class ProductViewModel
+    {
+        public AspProduct? Product { get; set; }
+
+        public string? SectionName { get; set; }
+
+        public string[]? ImagePathes { get; set; }
+    }
+}
