a815953ae1db1402a4d2ec3875626de84c863dfb
NonamePoc <pavloshaban@gmail.com>
2022-11-09 20:14:56 +0200

    Add basket
    Fix bugs,modificated home contoller,shop controller, view (basket,product,chekout)

--- CurbashaApi/Controllers/BasketProductController.cs
+++ CurbashaApi/Controllers/BasketProductController.cs
@@ -1,4 +1,6 @@
-﻿using CurbashaApi.Areas.Identity.Entity;
+﻿using System.Linq;
+using System.Threading.Tasks;
+using CurbashaApi.Areas.Identity.Entity;
 using CurbashaApi.Data;
 using CurbashaApi.Models;
 using CurbashaApi.ViewModel;
@@ -19,29 +21,94 @@ namespace CurbashaApi.Controllers
         [HttpGet]
         public IActionResult Basket()
         {
-            //ViewBag.count = _context.AspOrderItems.Where( w=> w.ProductId ).Select(s => s.Quantity);
-            var items = _context.AspOrderItems.ToList();
-            ViewBag.total = _context.AspOrderItems.Sum(s => s.Price);
+            var delivery = GetDelivery();
+
+            if (delivery == null || delivery.IsActive == false)
+            {
+                return RedirectToAction("Shop", "Shop");
+            }
+
+
+            var items = _context.AspOrderItems.Select(s => s).Where(w => w.OrderId == delivery.Id);
+
+            ViewBag.total = _context.AspOrderItems.Select(s => s).Where(w => w.OrderId == delivery.Id)
+                .Sum(s => s.Price * s.Quantity);
+
 
             return View(items);
-           
         }
 
         [HttpPost]
-        public IActionResult Basket(BasketViewModel model)
+        public IActionResult Basket(BasketViewModel model, int id)
         {
-          
-            var items = _context.AspOrderItems.ToList();
-            _context.AspOrderItems.RemoveRange(items);
+            var item = GetItem(id);
+            _context.AspOrderItems.Remove(item);
             _context.SaveChanges();
 
-            // asp order isactive false 
-            var order = _context.AspUserOrder.FirstOrDefault();
-            order.IsActive = false;
-            _context.SaveChanges();
 
-            return RedirectToAction("Basket");
+            return Json("Basket");
+        }
+
+        [HttpPost]
+        public IActionResult Checkout()
+        {
+            var delivery = GetDelivery();
+
+            ViewBag.total = _context.AspUserOrder.Select(s => s).Where(w => w.Id == delivery.Id).FirstOrDefault().Total;
+
+            return View(delivery);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ChangeOrderItemQuantity(int id, string symbol)
+        {
+            var orderItem = await _context.AspOrderItems.FirstOrDefaultAsync(f => f.Id == id);
+
+            if (orderItem is null)
+                return BadRequest("Order item not found");
+
+            if (orderItem.Quantity <= 1 && symbol == "-")
+                return Ok();
+
+            switch (symbol)
+            {
+                case "+":
+                    ++orderItem.Quantity;
+                    break;
+
+                case "-":
+                    --orderItem.Quantity;
+                    break;
+
+                default:
+                    return BadRequest("Symbol not valid");
+            }
+
+            _context.AspOrderItems.Update(orderItem);
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
+
+        private AspUserOrder GetDelivery()
+        {
+            var currentUser = _context.Users.FirstOrDefault(u => u.UserName == User.Identity.Name);
+
+            var basket = Request.Cookies["guest"];
+
+
+            if (currentUser == null)
+            {
+                currentUser = _context.Users.FirstOrDefault(u => u.Id == basket);
+            }
+
+            return _context.AspUserOrder.OrderBy(o => o.Id).LastOrDefault(u => u.User == currentUser);
+        }
+
+
+        private AspOrderItem GetItem(int id)
+        {
+            return _context.AspOrderItems.FirstOrDefault(i => i.Id == id);
         }
     }
 }
-
--- CurbashaApi/Controllers/HomeController.cs
+++ CurbashaApi/Controllers/HomeController.cs
@@ -1,20 +1,66 @@
 ﻿using CurbashaApi.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using CurbashaApi.Areas.Identity.Entity;
+using CurbashaApi.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.VisualBasic;
 
 namespace CurbashaApi.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly CurbashaApiContext _context;
+
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, CurbashaApiContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Home()
         {
+            var currentUser = _context.Users.FirstOrDefault(u => u.UserName == User.Identity.Name);
+
+            //Если у юзера нету аккаунта сделать ему аккаунт и присвоить ему id
+            if (currentUser == null)
+            {
+                var cookie = Request.Cookies["guest"];
+                if (cookie == null)
+                {
+                    var random = new Random();
+                    var guest = new IdentityUser()
+                    {
+                        UserName = "Guest" + random.Next(1000, 9999)
+                    };
+                    _context.Users.Add(guest);
+                    _context.SaveChanges();
+                    currentUser = guest;
+                    Response.Cookies.Append("guest", guest.Id);
+                }
+                else
+                {
+                    currentUser = _context.Users.FirstOrDefault(u => u.Id == cookie);
+                }
+            }
+
+
+            var delivery = _context.AspUserOrder.OrderBy(o => o.Id).LastOrDefault(u => u.User == currentUser);
+            if (delivery == null || delivery.IsActive == false)
+            {
+                var userOder = new AspUserOrder()
+                {
+                    CreateAt = DateAndTime.Now,
+                    IsActive = true,
+                    User = currentUser,
+                };
+
+                _context.AspUserOrder.Add(userOder);
+                _context.SaveChanges();
+                return RedirectToAction("Home");
+            }
 
             return View();
         }
